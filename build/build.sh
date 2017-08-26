470ae81f2817166478f77a87b3353e70b6a17ff7
James Kyle <me@thejameskyle.com>
2017-08-26 13:00:37 +1000

    Fix up build scripts

--- build/build.sh
+++
@@ -1,3 +0,0 @@
-rm -rf dist
-BUILD=legacy babel src -d dist/legacy --ignore "__tests__,__mocks__"
-babel src -d dist/modern --ignore "__tests__,__mocks__"
--- build/test.sh
+++
@@ -1,6 +0,0 @@
-EXIT_STATUS=0
-echo "$ flow status"
-yarn run flow status || EXIT_STATUS=$?
-echo "$ jest --coverage"
-yarn run jest -- --coverage || EXIT_STATUS=$?
-exit $EXIT_STATUS
