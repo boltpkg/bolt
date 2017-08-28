604b935c31963d446d16754ef6c6b477d2c05f28
Luke Batchelor <lbatchelor@atlassian.com>
2017-08-28 12:00:53 +1000

    Updates getPackages API to return more information about packages, rather than dirs

--- src/utils/getPackages.js
+++ src/utils/getPackages.js
@@ -5,5 +5,9 @@ export default async function getPackages(opts: { cwd?: string } = {}) {
   let cwd = opts.cwd || process.cwd();
   let project = await Project.init(cwd);
   let workspaces = await project.getWorkspaces();
-  return workspaces.map(workspace => workspace.pkg.dir);
+  return workspaces.map(workspace => ({
+    dir: workspace.pkg.dir,
+    name: workspace.pkg.name,
+    config: workspace.pkg.config,
+  }));
 }
