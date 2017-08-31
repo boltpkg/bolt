453bc1b9f112071343dcd6800f97a5b809a02c90
Luke Batchelor <lbatchelor@atlassian.com>
2017-08-31 12:37:26 +1000

    Adds getDependencyGraph utility function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 export {default as run} from './lib';
 export {default as cli} from './cli';
 export {default as getPackages} from './utils/getPackages';
+export {default as getDependencyGraph} from './utils/getDependencyGraph';
 export {default as runWorkspaceTasks} from './utils/runWorkspaceTasks';
 export {default as getUnpublishedPackages} from './utils/getUnpublishedPackages';
 export {default as updatePackageVersions} from './utils/updatePackageVersions';
---
+++ src/utils/getDependencyGraph.js
@@ -0,0 +1,22 @@
+// @flow
+import Project from '../Project';
+import * as yarn from '../utils/yarn';
+
+export default async function getDependencyGraph(opts: { cwd?: string } = {}) {
+  const cwd = process.cwd();
+  const project = await Project.init(cwd);
+  const workspaces = await project.getWorkspaces();
+
+  const {
+    graph: dependencyGraph,
+    valid: graphIsValid
+  } = await project.getDependencyGraph(workspaces);
+
+  if (!graphIsValid) throw new Error('Dependency graph is not valid');
+
+  let simplifiedDependencyGraph = new Map();
+
+  dependencyGraph.forEach((pkgInfo, pkgName) => {
+    simplifiedDependencyGraph.set(pkgName, pkgInfo.dependencies);
+  });
+}
