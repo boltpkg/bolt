3f3ae9e11d2fe3b8bef52613609c574193707d28
Luke.Batchelor <batchelor.luke+git@gmail.com>
2017-09-01 00:53:33 +1000

    Exposes filterWorkspaces command as util

--- src/commands/ws/run.js
+++ src/commands/ws/run.js
@@ -2,7 +2,7 @@
 import type {Args, Opts} from '../../types';
 import Project from '../../Project';
 import * as yarn from '../../utils/yarn';
-import filterWorkspaces from '../../utils/filterWorkspace';
+import filterWorkspaces from '../../utils/filterWorkspaces';
 
 export default async function run(args: Args, opts: Opts) {
   let cwd = process.cwd();
