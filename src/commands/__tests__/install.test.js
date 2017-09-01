4ee1e9294292112459d50f448977327059bf1532
James Kyle <me@thejameskyle.com>
2017-09-01 13:58:20 +1000

    Fix flow types

--- src/commands/__tests__/install.test.js
+++ src/commands/__tests__/install.test.js
@@ -2,11 +2,13 @@
 import install from '../install';
 import * as processes from '../../utils/processes';
 
+const unsafeProcesses: any = processes;
+
 jest.mock('../../utils/processes');
 jest.unmock('../install');
 
 test('pyarn install', async () => {
   await install([], {});
 
-  console.log(processes.spawn.mock.calls);
+  console.log(unsafeProcesses.spawn.mock.calls);
 });
