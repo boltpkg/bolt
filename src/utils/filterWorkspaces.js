462e89d295a5f9c672357cfa49e5829a42abd2f6
James Kyle <me@thejameskyle.com>
2017-09-04 13:19:29 +1000

    Update filterWorkspaces.js

--- src/utils/filterWorkspaces.js
+++ src/utils/filterWorkspaces.js
@@ -1,3 +1,4 @@
+// @flow
 import multimatch from 'multimatch';
 import path from 'path';
 
@@ -5,18 +6,23 @@ import type {Args, Opts} from '../types';
 import Workspace from '../Workspace';
 
 export default function filterWorkspaces(workspaces: [Workspace], opts: Opts) {
-    const onlyPattern = opts.only || '**';
-    const ignorePattern = opts.ignore ? `!${opts.ignore}` : '';
-    const onlyFsPattern = opts.onlyFs || '**';
-    const ignoreFsPattern = opts.ignoreFs ? `!${opts.ignoreFs}` : '';
-    let cwd = process.cwd();
+  const onlyPattern = opts.only || '**';
+  const ignorePattern = opts.ignore ? `!${opts.ignore}` : '';
+  const onlyFsPattern = opts.onlyFs || '**';
+  const ignoreFsPattern = opts.ignoreFs ? `!${opts.ignoreFs}` : '';
+  let cwd = process.cwd();
 
-    const relativeDir = workspace => path.relative(cwd, workspace.pkg.dir)
-    const workspaceNames = workspaces.map(workspace => workspace.pkg.config.name);
-    const workspaceDirs = workspaces.map(workspace => relativeDir(workspace));
+  const relativeDir = workspace => path.relative(cwd, workspace.pkg.dir)
+  const workspaceNames = workspaces.map(workspace => workspace.pkg.config.name);
+  const workspaceDirs = workspaces.map(workspace => relativeDir(workspace));
 
-    const filteredByName = multimatch(workspaceNames, [onlyPattern, ignorePattern]);
-    const filteredByDir = multimatch(workspaceDirs, [onlyFsPattern, ignoreFsPattern]);
+  const filteredByName = multimatch(workspaceNames, [onlyPattern, ignorePattern]);
+  const filteredByDir = multimatch(workspaceDirs, [onlyFsPattern, ignoreFsPattern]);
 
-    return workspaces.filter(workspace => filteredByName.includes(workspace.pkg.config.name) &&  filteredByDir.includes(relativeDir(workspace)));
+  return workspaces.filter(workspace => {
+    return (
+      filteredByName.includes(workspace.pkg.config.name) &&
+      filteredByDir.includes(relativeDir(workspace))
+    );
+  });
 }
