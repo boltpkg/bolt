c09717fdb5fd17efc8361d012812c583db324b8f
James Kyle <me@thejameskyle.com>
2017-09-06 11:09:53 +1000

    Run npm.info in getUnpublishedPackages in parallel

--- src/utils/getUnpublishedPackages.js
+++ src/utils/getUnpublishedPackages.js
@@ -6,19 +6,19 @@ export default async function getUnpublishedPackages(opts: { cwd?: string } = {}
   const cwd = opts.cwd || process.cwd();
   const project = await Project.init(cwd);
   const workspaces = await project.getWorkspaces();
-  const unpublishedPackages = [];
 
-  for (let workspace of workspaces) {
-    const packageInfo = await npm.info(workspace.pkg.config.name);
+  const results = await Promise.all(workspaces.map(async workspace => {
+    let config = workspace.pkg.config;
+    let packageInfo = await npm.info(config.name);
 
-    if (packageInfo.version !== workspace.pkg.config.version) {
-      unpublishedPackages.push({
-        name: packageInfo.name,
-        version: packageInfo.version,
-        previousVersion: workspace.pkg.config.version,
-      });
-    }
-  }
+    return {
+      name: packageInfo.name,
+      version: packageInfo.version,
+      previousVersion: config.version,
+    };
+  }));
 
-  return unpublishedPackages;
+  return results.filter(result => {
+    return result.version !== result.previousVersion;
+  });
 }
