d85a9434c85fd06a86767cc2276d11e3dca55c26
James Kyle <me@thejameskyle.com>
2017-09-06 12:07:14 +1000

    Move getUnpublishedPackages into publish command

--- src/commands/publish.js
+++ src/commands/publish.js
@@ -1,11 +1,31 @@
 // @flow
 import type {Args, Opts} from '../types';
 import {PError} from '../utils/errors';
-import getUnpublishedPackages from '../utils/getUnpublishedPackages';
 import * as logger from '../utils/logger';
 import * as locks from '../utils/locks';
 import * as npm from '../utils/npm';
 import Project from '../Project';
+
+async function getUnpublishedPackages(opts: { cwd?: string } = {}) {
+  const cwd = opts.cwd || process.cwd();
+  const project = await Project.init(cwd);
+  const workspaces = await project.getWorkspaces();
+
+  const results = await Promise.all(workspaces.map(async workspace => {
+    let config = workspace.pkg.config;
+    let packageInfo = await npm.info(config.name);
+
+    return {
+      name: packageInfo.name,
+      version: packageInfo.version,
+      previousVersion: config.version,
+    };
+  }));
+
+  return results.filter(result => {
+    return result.version !== result.previousVersion;
+  });
+}
 
 export default async function publish(args: Args, opts: Opts) {
   const cwd = typeof opts.cwd === 'string' ? opts.cwd : process.cwd();
@@ -27,7 +47,6 @@ export default async function publish(args: Args, opts: Opts) {
     });
 
     await locks.unlock(packages);
-
   } catch (err) {
     logger.error(err.message);
     throw new PError('Failed to publish');
