af93dbee5a2ada2bb21cb9b1b731604d28b74b2d
Luke Batchelor <lbatchelor@atlassian.com>
2017-09-08 21:05:19 +1000

    Adds Project.getDependentsGraph and tests

--- src/Project.js
+++ src/Project.js
@@ -1,15 +1,15 @@
 // @flow
-import * as path from 'path';
-import globby from 'globby';
+import * as path from "path";
+import globby from "globby";
 
-import Package from './Package';
-import Workspace from './Workspace';
-import type {Config} from './types';
-import {getProjectConfig} from './utils/config';
-import * as fs from './utils/fs';
-import * as logger from './utils/logger';
-import * as messages from './utils/messages';
-import {PError} from './utils/errors';
+import Package from "./Package";
+import Workspace from "./Workspace";
+import type { Config } from "./types";
+import { getProjectConfig } from "./utils/config";
+import * as fs from "./utils/fs";
+import * as logger from "./utils/logger";
+import * as messages from "./utils/messages";
+import { PError } from "./utils/errors";
 
 export type Task = (workspace: Workspace) => Promise<mixed>;
 
@@ -41,7 +41,7 @@ export default class Project {
         let stats = await fs.stat(dir);
         if (!stats.isDirectory()) continue;
 
-        let filePath = path.join(dir, 'package.json');
+        let filePath = path.join(dir, "package.json");
         let wPkg = await Package.init(filePath);
         let workspace = await Workspace.init(wPkg);
 
@@ -73,12 +73,19 @@ export default class Project {
         let match = packagesByName[depName];
         if (!match) continue;
 
-        let actual = depVersion.replace(/^\^/, '');
+        let actual = depVersion.replace(/^\^/, "");
         let expected = match.config.version;
 
         if (actual !== expected) {
           valid = false;
-          logger.error(messages.packageMustDependOnCurrentVersion(name, depName, expected, depVersion));
+          logger.error(
+            messages.packageMustDependOnCurrentVersion(
+              name,
+              depName,
+              expected,
+              depVersion
+            )
+          );
           continue;
         }
 
@@ -89,6 +96,41 @@ export default class Project {
     }
 
     return { graph, valid };
+  }
+
+  async getDependentsGraph(workspaces: Array<Workspace>) {
+    const graph = new Map();
+    const { valid, graph: dependencyGraph } = await this.getDependencyGraph(
+      workspaces
+    );
+
+    const dependentsLookup: {
+      [string]: { pkg: Package, dependents: Array<string> }
+    } = {};
+
+    workspaces.forEach(workspace => {
+      dependentsLookup[workspace.pkg.config.name] = {
+        pkg: workspace.pkg,
+        dependents: []
+      };
+    });
+
+    workspaces.forEach(workspace => {
+      const dependent = workspace.pkg.config.name;
+      const valFromDependencyGraph = dependencyGraph.get(dependent) || {};
+      const dependencies = valFromDependencyGraph.dependencies || [];
+
+      dependencies.forEach(dependency => {
+        dependentsLookup[dependency].dependents.push(dependent);
+      });
+    });
+
+    // can't use Object.entries here as the flow type for it is Array<[string, mixed]>;
+    Object.keys(dependentsLookup).forEach(key => {
+      graph.set(key, dependentsLookup[key]);
+    });
+
+    return { valid, graph };
   }
 
   // TODO: Properly sort packages using a topological sort, resolving cycles
---
+++ src/__tests__/Project.test.js
@@ -0,0 +1,92 @@
+// @flow
+import path from "path";
+import Project from "../Project";
+import Package from "../Package";
+import Workspace from "../Workspace";
+
+import { getFixturePath } from "jest-fixtures";
+
+describe("Project", () => {
+  let project;
+
+  describe("A simple project", async () => {
+    beforeEach(async () => {
+      const filePath = await getFixturePath(__dirname, "simple-project");
+      project = await Project.init(filePath);
+    });
+
+    it("should be able to create a simple project", async () => {
+      expect(project).toBeInstanceOf(Project);
+      expect(project.pkg).toBeInstanceOf(Package);
+    });
+
+    it("should be able to getWorkspaces", async () => {
+      const workspaces = await project.getWorkspaces();
+      expect(workspaces.length).toEqual(2);
+      expect(workspaces[0]).toBeInstanceOf(Workspace);
+    });
+
+    it("should be able to runWorkspaceTasks", async () => {
+      const workspaces = await project.getWorkspaces();
+      const spy = jest.fn(() => Promise.resolve());
+
+      Project.runWorkspaceTasks(workspaces, spy);
+
+      expect(spy).toHaveBeenCalledTimes(2);
+      // should be called with our workspace
+      expect(spy.mock.calls[0][0]).toBeInstanceOf(Workspace);
+    });
+  });
+
+  describe("A more complex Project", () => {
+    beforeEach(async () => {
+      const filePath = await getFixturePath(__dirname, "nested-workspaces");
+      project = await Project.init(filePath);
+    });
+
+    it("should be able to getWorkspaces (including nested)", async () => {
+      const workspaces = await project.getWorkspaces();
+      expect(workspaces.length).toEqual(3);
+      expect(workspaces[0]).toBeInstanceOf(Workspace);
+    });
+
+    it("should be able to getDependencyGraph", async () => {
+      const workspaces = await project.getWorkspaces();
+      const deps = await project.getDependencyGraph(workspaces);
+      const { valid, graph } = deps;
+
+      expect(valid).toEqual(true);
+      expect(graph).toBeInstanceOf(Map);
+
+      /**
+       Need to figure out if this workspace package should be included here or not, currently it is.
+      */
+      expect(graph.size).toBe(4);
+      const expectDependencies = (key, len) => {
+        const val = graph.get(key);
+        expect(val && val.dependencies && val.dependencies.length).toEqual(len);
+      };
+      expectDependencies("bar", 0);
+      expectDependencies("foo", 1);
+      expectDependencies("baz", 1);
+    });
+
+    it("should be able to getDependentsGraph", async () => {
+      const workspaces = await project.getWorkspaces();
+      const deps = await project.getDependentsGraph(workspaces);
+
+      const { valid, graph } = deps;
+      expect(valid).toEqual(true);
+      expect(graph).toBeInstanceOf(Map);
+      expect(graph.size).toBe(3);
+
+      const expectDependents = (key, len) => {
+        const val = graph.get(key);
+        expect(val && val.dependents && val.dependents.length).toEqual(len);
+      };
+      expectDependents("bar", 2);
+      expectDependents("foo", 0);
+      expectDependents("baz", 0);
+    });
+  });
+});
