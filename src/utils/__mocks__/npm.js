adda5d5b4836c2927fe2852cc8e44b84180ae180
Luke Batchelor <lbatchelor@atlassian.com>
2017-09-08 18:06:50 +1000

    Adds tests for publish command

---
+++ src/utils/__mocks__/npm.js
@@ -0,0 +1,37 @@
+const actualSpawn = require.requireActual('../npm');
+const npm = jest.genMockFromModule('../npm');
+
+
+let mockInfoResponses = [];
+
+npm.info = async function(pkgName) {
+  return Promise.resolve({
+    name: pkgName,
+    version: '1.0.0',
+  });
+};
+
+npm.infoAllow404 = function(pkgName) {
+  const mockResponse = mockInfoResponses.find(mock => mock.pkgName === pkgName);
+
+  if (mockResponse) return Promise.resolve(mockResponse.mockResponse);
+
+  return Promise.resolve({
+    response: '200',
+    data: {
+      name: pkgName,
+      version: '1.0.0',
+    }
+  });
+};
+
+/** Causes npm.infoAllow404 to return a specified mock response for a packageName */
+npm.__mockInfoAllow404 = (pkgName, mockResponse) => {
+  mockInfoResponses.push({pkgName, mockResponse});
+};
+
+npm.__clearMockInfoAllow404 = () => {
+  mockInfoResponses = [];
+};
+
+module.exports = npm;
