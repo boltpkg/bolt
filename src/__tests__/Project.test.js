5ed33146202c689fd2fd39a65a1b06d86cd1aab7
Luke Batchelor <lbatchelor@atlassian.com>
2017-09-11 13:09:14 +1000

    Adds single-quote rule to prettier config

--- src/__tests__/Project.test.js
+++ src/__tests__/Project.test.js
@@ -1,32 +1,32 @@
 // @flow
-import path from "path";
-import Project from "../Project";
-import Package from "../Package";
-import Workspace from "../Workspace";
+import path from 'path';
+import Project from '../Project';
+import Package from '../Package';
+import Workspace from '../Workspace';
 
-import { getFixturePath } from "jest-fixtures";
+import { getFixturePath } from 'jest-fixtures';
 
-describe("Project", () => {
+describe('Project', () => {
   let project;
 
-  describe("A simple project", async () => {
+  describe('A simple project', async () => {
     beforeEach(async () => {
-      const filePath = await getFixturePath(__dirname, "simple-project");
+      const filePath = await getFixturePath(__dirname, 'simple-project');
       project = await Project.init(filePath);
     });
 
-    it("should be able to create a simple project", async () => {
+    it('should be able to create a simple project', async () => {
       expect(project).toBeInstanceOf(Project);
       expect(project.pkg).toBeInstanceOf(Package);
     });
 
-    it("should be able to getWorkspaces", async () => {
+    it('should be able to getWorkspaces', async () => {
       const workspaces = await project.getWorkspaces();
       expect(workspaces.length).toEqual(2);
       expect(workspaces[0]).toBeInstanceOf(Workspace);
     });
 
-    it("should be able to runWorkspaceTasks", async () => {
+    it('should be able to runWorkspaceTasks', async () => {
       const workspaces = await project.getWorkspaces();
       const spy = jest.fn(() => Promise.resolve());
 
@@ -38,19 +38,19 @@ describe("Project", () => {
     });
   });
 
-  describe("A more complex Project", () => {
+  describe('A more complex Project', () => {
     beforeEach(async () => {
-      const filePath = await getFixturePath(__dirname, "nested-workspaces");
+      const filePath = await getFixturePath(__dirname, 'nested-workspaces');
       project = await Project.init(filePath);
     });
 
-    it("should be able to getWorkspaces (including nested)", async () => {
+    it('should be able to getWorkspaces (including nested)', async () => {
       const workspaces = await project.getWorkspaces();
       expect(workspaces.length).toEqual(3);
       expect(workspaces[0]).toBeInstanceOf(Workspace);
     });
 
-    it("should be able to getDependencyGraph", async () => {
+    it('should be able to getDependencyGraph', async () => {
       const workspaces = await project.getWorkspaces();
       const deps = await project.getDependencyGraph(workspaces);
       const { valid, graph } = deps;
@@ -63,12 +63,12 @@ describe("Project", () => {
         const val = graph.get(key);
         expect(val && val.dependencies && val.dependencies.length).toEqual(len);
       };
-      expectDependencies("bar", 0);
-      expectDependencies("foo", 1);
-      expectDependencies("baz", 1);
+      expectDependencies('bar', 0);
+      expectDependencies('foo', 1);
+      expectDependencies('baz', 1);
     });
 
-    it("should be able to getDependentsGraph", async () => {
+    it('should be able to getDependentsGraph', async () => {
       const workspaces = await project.getWorkspaces();
       const deps = await project.getDependentsGraph(workspaces);
 
@@ -81,9 +81,9 @@ describe("Project", () => {
         const val = graph.get(key);
         expect(val && val.dependents && val.dependents.length).toEqual(len);
       };
-      expectDependents("bar", 2);
-      expectDependents("foo", 0);
-      expectDependents("baz", 0);
+      expectDependents('bar', 2);
+      expectDependents('foo', 0);
+      expectDependents('baz', 0);
     });
   });
 });
