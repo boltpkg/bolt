623e2af7bfc64e50a16bdb80e84bd570ec4a07eb
Luke Batchelor <lbatchelor@atlassian.com>
2017-09-14 15:11:02 +1000

    Aligns getDependentsGraph tests with new Project tests

--- src/functions/__tests__/getDependentsGraph.test.js
+++ src/functions/__tests__/getDependentsGraph.test.js
@@ -5,14 +5,23 @@ import getDependentsGraph from '../getDependentsGraph';
 import { getFixturePath } from 'jest-fixtures';
 
 describe('function/getDependentsGraph', () => {
-  describe('A simple project', async () => {
+  describe('A project with nested workspaces', async () => {
     it('should return a simplified dependents graph', async () => {
       const cwd = await getFixturePath(__dirname, 'nested-workspaces');
       const dependentsGraph = await getDependentsGraph({ cwd });
+      const expectedDependents = {
+        bar: ['foo', 'baz'],
+        foo: [],
+        baz: []
+      };
 
-      expect(dependentsGraph.get('bar')).toEqual(['foo', 'baz']);
-      expect(dependentsGraph.get('foo')).toEqual([]);
-      expect(dependentsGraph.get('baz')).toEqual([]);
+      expect(dependentsGraph.size).toEqual(
+        Object.keys(expectedDependents).length
+      );
+      Object.entries(expectedDependents).forEach(([pkg, dependents]) => {
+        expect(dependentsGraph.get(pkg)).toEqual(dependents);
+      });
+      console.log(dependentsGraph);
     });
   });
 });
