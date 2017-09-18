72f9d0bb056de038020ec3515c08b6f9748e7f0b
James Kyle <me@thejameskyle.com>
2017-09-18 18:10:15 +1000

    Add pyarn project run

--- src/commands/project/run.js
+++ src/commands/project/run.js
@@ -1,13 +1,30 @@
 // @flow
+import Project from '../../Project';
 import * as options from '../../utils/options';
+import * as yarn from '../../utils/yarn';
 import {PError} from '../../utils/errors';
 
-export type ProjectRunOptions = {};
+export type ProjectRunOptions = {
+  cwd?: string,
+  script: string,
+  scriptArgs: options.Args,
+};
 
 export function toProjectRunOptions(args: options.Args, flags: options.Flags): ProjectRunOptions {
-  return {};
+  let [script, ...scriptArgs] = args;
+  return {
+    cwd: options.string(flags.cwd, 'cwd'),
+    script,
+    scriptArgs,
+  };
 }
 
 export async function projectRun(opts: ProjectRunOptions) {
-  throw new PError('Unimplemented command "project run"');
+  let cwd = opts.cwd || process.cwd();
+  let project = await Project.init(cwd);
+  let validScript = await yarn.run(project.pkg, opts.script, opts.scriptArgs);
+
+  if (!validScript) {
+    throw new PError(`Package at "${project.pkg.dir}" does not have a script named "${opts.script}"`);
+  }
 }
