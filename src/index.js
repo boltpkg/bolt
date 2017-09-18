0debc213144b4ef1d63c03cbe6dff04c03d78a2c
lukebatchelor <batchelor.luke+github@gmail.com>
2017-09-18 12:18:44 +1000

    Exposes getDependentsGraph public function (#18)
    * Exposes getDependentsGraph public function
    
    * Adds more tests for Project functions
    
    * Removes the root package from the dependents graph
    
    * Aligns getDependentsGraph tests with new Project tests
    
    * Removes trailing console statement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 // @flow
-export {default as getWorkspaces} from './functions/getWorkspaces';
-export {default as getDependencyGraph} from './functions/getDependencyGraph';
-export {default as runWorkspaceTasks} from './functions/runWorkspaceTasks';
-export {default as updatePackageVersions} from './functions/updatePackageVersions';
+export { default as getWorkspaces } from './functions/getWorkspaces';
+export { default as getDependencyGraph } from './functions/getDependencyGraph';
+export { default as getDependentsGraph } from './functions/getDependentsGraph';
+export { default as runWorkspaceTasks } from './functions/runWorkspaceTasks';
+export {
+  default as updatePackageVersions
+} from './functions/updatePackageVersions';
