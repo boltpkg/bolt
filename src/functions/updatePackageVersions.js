943c365e8ff0d5f20098c63966311934d63248bb
Luke Batchelor <lbatchelor@atlassian.com>
2017-09-19 18:53:46 +1000

    Returns list of updated packages in updatePackageVersions

--- src/functions/updatePackageVersions.js
+++ src/functions/updatePackageVersions.js
@@ -3,17 +3,21 @@ import Project from '../Project';
 import Workspace from '../Workspace';
 
 type VersionMap = {
-  [name: string]: string,
+  [name: string]: string
 };
 
 type Options = {
-  cwd?: string,
+  cwd?: string
 };
 
-export default async function updatePackageVersions(versions: VersionMap, opts: Options = {}): Promise<void> {
+export default async function updatePackageVersions(
+  versions: VersionMap,
+  opts: Options = {}
+): Promise<Array<string>> {
   let cwd = opts.cwd || process.cwd();
   let project = await Project.init(cwd);
   let workspaces = await project.getWorkspaces();
+  let updatedPackages = new Set();
 
   for (let workspace of workspaces) {
     let pkg = workspace.pkg;
@@ -22,9 +26,18 @@ export default async function updatePackageVersions(versions: VersionMap, opts: 
     for (let depName of Object.keys(versions)) {
       let depType = pkg.getDependencyType(depName);
       if (!depType) continue;
-      promises.push(pkg.updateDependencyVersionRange(depName, depType, '^' + versions[depName]));
+      promises.push(
+        pkg.updateDependencyVersionRange(
+          depName,
+          depType,
+          '^' + versions[depName]
+        )
+      );
+      updatedPackages.add(pkg.filePath);
     }
 
     await Promise.all(promises);
   }
+
+  return Array.from(updatedPackages);
 }
