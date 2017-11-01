19533dca4adc4946ff270f3667342a89c791531e
Luke Batchelor <lbatchelor@atlassian.com>
2017-11-01 14:04:12 +1100

    Adds engines.bolt check to validateProject()

--- src/utils/validateProject.js
+++ src/utils/validateProject.js
@@ -1,18 +1,33 @@
 // @flow
-
+import semver from 'semver';
 import Project from '../Project';
+import Config from '../Config';
 import type Workspace from '../Workspace';
 import type Package from '../Package';
 import * as messages from './messages';
 import { BoltError } from './errors';
 import * as logger from './logger';
+import { BOLT_VERSION } from '../constants';
 
 export default async function validateProject(project: Project) {
   const workspaces = await project.getWorkspaces();
   const projectDependencies = project.pkg.getAllDependencies();
+  const projectConfig = project.pkg.config;
   const { graph: depGraph } = await project.getDependencyGraph(workspaces);
 
   let projectIsValid = true;
+
+  // If the project has an engines.bolt field we must respect it
+  const enginesConfig = projectConfig.getEngines();
+  if (enginesConfig && enginesConfig.bolt) {
+    const expectedBoltVersion = enginesConfig.bolt;
+    if (!semver.satisfies(BOLT_VERSION, expectedBoltVersion)) {
+      logger.error(
+        messages.invalidBoltVersion(BOLT_VERSION, expectedBoltVersion)
+      );
+      projectIsValid = false;
+    }
+  }
 
   // Workspaces should never appear as dependencies in the Project config
   for (let workspace of workspaces) {
