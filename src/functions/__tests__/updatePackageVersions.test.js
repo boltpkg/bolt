6a71e401058234eaa771db8874bd502e73f4179b
Ben Conolly <ben@thinkmill.com.au>
2017-11-15 17:02:02 +1100

    Skip updating workspaces not in updated packages (#64)
    * skip updating workspaces not in updated packages
    
    * throw error if invalid udpatedPackages
    
    * correct includes
    
    * remove unnecessary function

--- src/functions/__tests__/updatePackageVersions.test.js
+++ src/functions/__tests__/updatePackageVersions.test.js
@@ -1,12 +1,18 @@
 // @flow
 import path from 'path';
+import { copyFixtureIntoTempDir } from 'jest-fixtures';
+
 import updatePackageVersions from '../updatePackageVersions';
 import * as fs from '../../utils/fs';
 import * as logger from '../../utils/logger';
 import Config from '../../Config';
-import { copyFixtureIntoTempDir } from 'jest-fixtures';
-
+import * as messages from '../../utils/messages';
 jest.mock('../../utils/logger');
+
+/*
+* NOTE: The way we are testing, we are testing the update of a single dependency.
+* This means that projects will be left in an invalid state after most test runs.
+*/
 
 async function getDepVersion(
   pathToPkg: string,
@@ -33,38 +39,43 @@ describe('function/updatePackageVersions', () => {
     });
 
     it('should update internal deps with caret deps', async () => {
+      const updatedPackages = { 'caret-dep': '1.2.0', 'has-all-deps': '1.1.2' };
       expect(await getDepVersion(pkgWithDepsPath, 'caret-dep')).toBe('^1.1.1');
       // pretend we've just updated 'caret-dep' to 2.0.0
-      await updatePackageVersions({ 'caret-dep': '2.0.0' }, { cwd });
+      await updatePackageVersions(updatedPackages, { cwd });
 
-      expect(await getDepVersion(pkgWithDepsPath, 'caret-dep')).toBe('^2.0.0');
+      expect(await getDepVersion(pkgWithDepsPath, 'caret-dep')).toBe('^1.2.0');
     });
 
     it('should update internal deps with tilde deps', async () => {
+      const updatePackages = { 'tilde-dep': '2.0.0', 'has-all-deps': '1.1.2' };
       expect(await getDepVersion(pkgWithDepsPath, 'tilde-dep')).toBe('~1.1.1');
       // pretend we've just updated 'tilde-dep' to 2.0.0
-      await updatePackageVersions({ 'tilde-dep': '2.0.0' }, { cwd });
+      await updatePackageVersions(updatePackages, { cwd });
 
       expect(await getDepVersion(pkgWithDepsPath, 'tilde-dep')).toBe('~2.0.0');
     });
 
     it('should update internal deps with pinned deps', async () => {
+      const updatePackages = { 'pinned-dep': '2.0.0', 'has-all-deps': '1.1.2' };
       expect(await getDepVersion(pkgWithDepsPath, 'pinned-dep')).toBe('1.1.1');
       // pretend we've just updated 'pinned-dep' to 2.0.0
-      await updatePackageVersions({ 'pinned-dep': '2.0.0' }, { cwd });
+      await updatePackageVersions(updatePackages, { cwd });
 
       expect(await getDepVersion(pkgWithDepsPath, 'pinned-dep')).toBe('2.0.0');
     });
 
     it('should return list of updated packages', async () => {
-      const updated = await updatePackageVersions(
-        { 'caret-dep': '2.0.0' },
-        { cwd }
-      );
+      const updatePackages = {
+        'caret-dep': '1.2.0',
+        'has-all-deps': '1.1.2',
+        'pinned-dep': '1.2.2'
+      };
+
+      const updated = await updatePackageVersions(updatePackages, { cwd });
       const expectedUpdated = [
         'packages/has-all-deps/package.json',
-        'packages/pinned-dep/package.json',
-        'packages/has-all-deps-with-peers/package.json'
+        'packages/pinned-dep/package.json'
       ];
 
       expect(updated.length).toEqual(expectedUpdated.length);
@@ -88,7 +99,11 @@ describe('function/updatePackageVersions', () => {
       expect(await getDepVersion(pkgWithDepsPath, 'tilde-dep')).toBe('~1.1.1');
 
       await updatePackageVersions(
-        { 'pinned-dep': '2.0.0', 'tilde-dep': '2.0.0' },
+        {
+          'pinned-dep': '2.0.0',
+          'tilde-dep': '2.0.0',
+          'has-all-deps': '1.1.2'
+        },
         { cwd }
       );
 
@@ -98,20 +113,50 @@ describe('function/updatePackageVersions', () => {
 
     it('should update a peerDep and devDep together', async () => {
       expect(
-        await getDepVersion(pkgPeerDepsPath, 'pinned-dep', 'devDependencies')
-      ).toBe('1.1.1');
+        await getDepVersion(pkgPeerDepsPath, 'caret-dep', 'devDependencies')
+      ).toBe('^1.1.1');
       expect(
-        await getDepVersion(pkgPeerDepsPath, 'pinned-dep', 'peerDependencies')
-      ).toBe('1.1.1');
+        await getDepVersion(pkgPeerDepsPath, 'caret-dep', 'peerDependencies')
+      ).toBe('^1.1.1');
 
-      await updatePackageVersions({ 'pinned-dep': '2.0.0' }, { cwd });
+      await updatePackageVersions(
+        { 'caret-dep': '1.2.0', 'has-all-deps-with-peers': '1.1.2' },
+        { cwd }
+      );
 
       expect(
-        await getDepVersion(pkgPeerDepsPath, 'pinned-dep', 'devDependencies')
-      ).toBe('2.0.0');
+        await getDepVersion(pkgPeerDepsPath, 'caret-dep', 'devDependencies')
+      ).toBe('^1.2.0');
       expect(
-        await getDepVersion(pkgPeerDepsPath, 'pinned-dep', 'peerDependencies')
-      ).toBe('2.0.0');
+        await getDepVersion(pkgPeerDepsPath, 'caret-dep', 'peerDependencies')
+      ).toBe('^1.2.0');
+    });
+    it('should skip updating dependencies for packages not in the udpatedPackages', async () => {
+      const updatePackages = { 'caret-dep': '1.2.0' };
+      expect(
+        await getDepVersion(pkgPeerDepsPath, 'caret-dep', 'peerDependencies')
+      ).toBe('^1.1.1');
+      await updatePackageVersions(updatePackages, { cwd });
+
+      // This tests that peer deps not to be updated even though it has a dependency
+      // as our passed updatePackages is not requesting its release.
+      expect(
+        await getDepVersion(pkgPeerDepsPath, 'caret-dep', 'peerDependencies')
+      ).toBe('^1.1.1');
+    });
+    it('should throw when you will leave a version range without it being included', async () => {
+      const updatePackages = { 'tilde-dep': '2.0.0' };
+      const message = messages.invalidBoltWorkspacesFromUpdate(
+        'has-all-deps',
+        'tilde-dep',
+        '~1.1.1',
+        '2.0.0'
+      );
+      return updatePackageVersions(updatePackages, {
+        cwd
+      }).catch(returnedMessage => {
+        expect(returnedMessage.message).toEqual(message);
+      });
     });
   });
 });
--- src/functions/updatePackageVersions.js
+++ src/functions/updatePackageVersions.js
@@ -1,8 +1,11 @@
 // @flow
+import semver from 'semver';
+
 import Project from '../Project';
 import Workspace from '../Workspace';
 import * as logger from '../utils/logger';
 import * as messages from '../utils/messages';
+import includes from 'array-includes';
 
 type VersionMap = {
   [name: string]: string
@@ -20,28 +23,35 @@ function versionRangeToRangeType(versionRange: string) {
 
 /**
  * This function is used to update all the internal dependencies where you have an external source
- * bumping versions (a tool like bolt-releases for example).
- * It takes an object of packageNames and their new versions. updatePackageVersions will update all
- * internal versions of packages according to those new versions.
+ * bumping updated packages (a tool like bolt-releases for example).
+ * It takes an object of packageNames and their new updated packages. updatePackageVersions will update all
+ * internal updated packages of packages according to those new updated packages.
  * ie, a caret dep, will remain a caret dep and a pinned dep will remain pinned.
  *
- * It is up to the consumer to ensure that these new versions are not going to leave the repo in an
- * inconsistent state (internal deps leaving semver ranges).
- *
  * Note: we explicitly ignore all external dependencies passed and warn if they are.
+ *
+ * It is up to the consumer to ensure that these new updated packages are not going to leave the repo in an
+ * inconsistent state (internal deps leaving semver ranges). This can occur if your
+ * updated packages will not release all packages that need to be.
+ *
  */
+
 export default async function updatePackageVersions(
-  versions: VersionMap,
+  updatedPackages: VersionMap,
   opts: Options = {}
 ): Promise<Array<string>> {
   let cwd = opts.cwd || process.cwd();
   let project = await Project.init(cwd);
   let workspaces = await project.getWorkspaces();
   let { graph } = await project.getDependencyGraph(workspaces);
-  let updatedPackages = new Set();
+  let editedPackages = new Set();
 
-  const internalDeps = Object.keys(versions).filter(dep => graph.has(dep));
-  const externalDeps = Object.keys(versions).filter(dep => !graph.has(dep));
+  const internalDeps = Object.keys(updatedPackages).filter(dep =>
+    graph.has(dep)
+  );
+  const externalDeps = Object.keys(updatedPackages).filter(
+    dep => !graph.has(dep)
+  );
 
   if (externalDeps.length !== 0) {
     logger.warn(
@@ -52,20 +62,40 @@ export default async function updatePackageVersions(
   for (let workspace of workspaces) {
     let pkg = workspace.pkg;
     let promises = [];
+    let name = workspace.pkg.config.getName();
 
     for (let depName of internalDeps) {
       const depRange = String(pkg.getDependencyVersionRange(depName));
       const depTypes = pkg.getDependencyTypes(depName);
       const rangeType = versionRangeToRangeType(depRange);
-      const newDepRange = rangeType + versions[depName];
-
+      const newDepRange = rangeType + updatedPackages[depName];
       if (depTypes.length === 0) continue;
+
+      const inUpdatedPackages = includes(internalDeps, name);
+      const willLeaveSemverRange = !semver.satisfies(
+        updatedPackages[depName],
+        depRange
+      );
+      // This check determines whether the package will be released. If the
+      // package will not be released, we throw.
+      if (!inUpdatedPackages && willLeaveSemverRange) {
+        throw new Error(
+          messages.invalidBoltWorkspacesFromUpdate(
+            name,
+            depName,
+            depRange,
+            updatedPackages[depName]
+          )
+        );
+      }
+      if (!inUpdatedPackages) continue;
+
       for (let depType of depTypes) {
         await pkg.setDependencyVersionRange(depName, depType, newDepRange);
       }
-      updatedPackages.add(pkg.filePath);
+      editedPackages.add(pkg.filePath);
     }
   }
 
-  return Array.from(updatedPackages);
+  return Array.from(editedPackages);
 }
