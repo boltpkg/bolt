4639f83a247a433d88193776a4636580e7a4117d
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2017-12-31 11:17:15 +1100

    updated test cases

--- src/commands/__tests__/init.test.js
+++ src/commands/__tests__/init.test.js
@@ -1,4 +1,47 @@
 // @flow
 import { init, toInitOptions } from '../init';
+import * as yarn from '../../utils/yarn';
+import { copyFixtureIntoTempDir } from 'jest-fixtures';
 
-test('bolt init');
+jest.mock('../../utils/yarn');
+
+describe('bolt init', () => {
+  let projectDir;
+
+  beforeEach(async () => {
+    projectDir = await copyFixtureIntoTempDir(
+      __dirname,
+      'package-with-external-deps-installed'
+    );
+  });
+
+  it('should be able to pass yes to yarn when y flag is passed', async () => {
+    await init(toInitOptions([], { cwd: projectDir, y: true }));
+    expect(yarn.init).toHaveBeenCalledWith(projectDir, { yes: true });
+  });
+
+  it('should be able to pass yes to yarn when yes flag is passes', async () => {
+    await init(toInitOptions([], { cwd: projectDir, yes: true }));
+    expect(yarn.init).toHaveBeenCalledWith(projectDir, { yes: true });
+  });
+
+  it('should be able to pass private to yarn when p flag is passed', async () => {
+    await init(toInitOptions([], { cwd: projectDir, p: true }));
+    expect(yarn.init).toHaveBeenCalledWith(projectDir, { private: true });
+  });
+
+  it('should be able to pass private to yarn when private flag is passes', async () => {
+    await init(toInitOptions([], { cwd: projectDir, private: true }));
+    expect(yarn.init).toHaveBeenCalledWith(projectDir, { private: true });
+  });
+
+  it('should be able to pass yes and private to yarn when private and yes flag is passes', async () => {
+    await init(
+      toInitOptions([], { cwd: projectDir, private: true, yes: true })
+    );
+    expect(yarn.init).toHaveBeenCalledWith(projectDir, {
+      private: true,
+      yes: true
+    });
+  });
+});
