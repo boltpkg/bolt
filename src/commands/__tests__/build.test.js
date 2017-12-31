928acc6150f6c3de4ae0eca64a07dd08c42b8859
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2018-01-01 00:03:00 +1100

    added some test cases to for aditional flags passed to scripts

--- src/commands/__tests__/build.test.js
+++ src/commands/__tests__/build.test.js
@@ -6,15 +6,6 @@ import * as boltRun from '../run';
 jest.mock('../run');
 
 describe('bolt build', () => {
-  let projectDir;
-
-  beforeEach(async () => {
-    projectDir = await copyFixtureIntoTempDir(
-      __dirname,
-      'package-with-external-deps-installed'
-    );
-  });
-
   it('should call run with build script with no flags or arguments', async () => {
     await build(toBuildOptions([], { '--': [] }));
     expect(boltRun.run).toHaveBeenCalledWith({
--- src/utils/__tests__/options.test.js
+++ src/utils/__tests__/options.test.js
@@ -4,4 +4,21 @@ import * as options from '../options';
 describe('options', () => {
   test('string');
   test('boolean');
+
+  describe('toScriptFlags', () => {
+    it('returns empty array if there were no custom flags passes', async () => {
+      const scriptFlags = await options.toScriptFlags({
+        '--': []
+      });
+      expect(scriptFlags).toEqual([]);
+    });
+    it('returns flags with value with the custom flags that are passed', async () => {
+      const scriptFlags = await options.toScriptFlags({
+        '--': [],
+        coverage: true,
+        target: 'Node9'
+      });
+      expect(scriptFlags).toEqual(['--coverage', '--target=Node9']);
+    });
+  });
 });
