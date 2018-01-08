de207bf72938fc8140fd03bad18c342a7e4dc3b7
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2018-01-08 20:54:10 +1100

    update init test cases

--- src/commands/__tests__/init.test.js
+++ src/commands/__tests__/init.test.js
@@ -17,31 +17,44 @@ describe('bolt init', () => {
 
   it('should be able to pass yes to yarn when y flag is passed', async () => {
     await init(toInitOptions([], { cwd: projectDir, y: true }));
-    expect(yarn.init).toHaveBeenCalledWith(projectDir, { yes: true });
+    expect(yarn.cliCommand).toHaveBeenCalledWith(projectDir, 'init', [
+      '-s',
+      '-y'
+    ]);
   });
 
   it('should be able to pass yes to yarn when yes flag is passes', async () => {
     await init(toInitOptions([], { cwd: projectDir, yes: true }));
-    expect(yarn.init).toHaveBeenCalledWith(projectDir, { yes: true });
+    expect(yarn.cliCommand).toHaveBeenCalledWith(projectDir, 'init', [
+      '-s',
+      '-y'
+    ]);
   });
 
   it('should be able to pass private to yarn when p flag is passed', async () => {
     await init(toInitOptions([], { cwd: projectDir, p: true }));
-    expect(yarn.init).toHaveBeenCalledWith(projectDir, { private: true });
+    expect(yarn.cliCommand).toHaveBeenCalledWith(projectDir, 'init', [
+      '-s',
+      '-p'
+    ]);
   });
 
   it('should be able to pass private to yarn when private flag is passes', async () => {
     await init(toInitOptions([], { cwd: projectDir, private: true }));
-    expect(yarn.init).toHaveBeenCalledWith(projectDir, { private: true });
+    expect(yarn.cliCommand).toHaveBeenCalledWith(projectDir, 'init', [
+      '-s',
+      '-p'
+    ]);
   });
 
   it('should be able to pass yes and private to yarn when private and yes flag is passes', async () => {
     await init(
       toInitOptions([], { cwd: projectDir, private: true, yes: true })
     );
-    expect(yarn.init).toHaveBeenCalledWith(projectDir, {
-      private: true,
-      yes: true
-    });
+    expect(yarn.cliCommand).toHaveBeenCalledWith(projectDir, 'init', [
+      '-s',
+      '-p',
+      '-y'
+    ]);
   });
 });
