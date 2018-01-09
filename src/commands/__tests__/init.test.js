ec30c534c1a607a0102509cb7ee7a2cab627c6d8
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2018-01-09 12:01:39 +1100

    updated init tests and reverted gitignore for node_modules in fixtures

--- src/commands/__tests__/init.test.js
+++ src/commands/__tests__/init.test.js
@@ -1,57 +1,45 @@
 // @flow
 import { init, toInitOptions } from '../init';
 import * as yarn from '../../utils/yarn';
-import { copyFixtureIntoTempDir } from 'jest-fixtures';
 
 jest.mock('../../utils/yarn');
-
+const dummyPath = '/dummyPattern/dummyPath';
 describe('bolt init', () => {
-  let projectDir;
-
-  beforeEach(async () => {
-    projectDir = await copyFixtureIntoTempDir(
-      __dirname,
-      'package-with-external-deps-installed'
-    );
-  });
-
   it('should be able to pass yes to yarn when y flag is passed', async () => {
-    await init(toInitOptions([], { cwd: projectDir, y: true }));
-    expect(yarn.cliCommand).toHaveBeenCalledWith(projectDir, 'init', [
+    await init(toInitOptions([], { cwd: dummyPath, y: true }));
+    expect(yarn.cliCommand).toHaveBeenCalledWith(dummyPath, 'init', [
       '-s',
       '-y'
     ]);
   });
 
   it('should be able to pass yes to yarn when yes flag is passes', async () => {
-    await init(toInitOptions([], { cwd: projectDir, yes: true }));
-    expect(yarn.cliCommand).toHaveBeenCalledWith(projectDir, 'init', [
+    await init(toInitOptions([], { cwd: dummyPath, yes: true }));
+    expect(yarn.cliCommand).toHaveBeenCalledWith(dummyPath, 'init', [
       '-s',
       '-y'
     ]);
   });
 
   it('should be able to pass private to yarn when p flag is passed', async () => {
-    await init(toInitOptions([], { cwd: projectDir, p: true }));
-    expect(yarn.cliCommand).toHaveBeenCalledWith(projectDir, 'init', [
+    await init(toInitOptions([], { cwd: dummyPath, p: true }));
+    expect(yarn.cliCommand).toHaveBeenCalledWith(dummyPath, 'init', [
       '-s',
       '-p'
     ]);
   });
 
   it('should be able to pass private to yarn when private flag is passes', async () => {
-    await init(toInitOptions([], { cwd: projectDir, private: true }));
-    expect(yarn.cliCommand).toHaveBeenCalledWith(projectDir, 'init', [
+    await init(toInitOptions([], { cwd: dummyPath, private: true }));
+    expect(yarn.cliCommand).toHaveBeenCalledWith(dummyPath, 'init', [
       '-s',
       '-p'
     ]);
   });
 
   it('should be able to pass yes and private to yarn when private and yes flag is passes', async () => {
-    await init(
-      toInitOptions([], { cwd: projectDir, private: true, yes: true })
-    );
-    expect(yarn.cliCommand).toHaveBeenCalledWith(projectDir, 'init', [
+    await init(toInitOptions([], { cwd: dummyPath, private: true, yes: true }));
+    expect(yarn.cliCommand).toHaveBeenCalledWith(dummyPath, 'init', [
       '-s',
       '-p',
       '-y'
