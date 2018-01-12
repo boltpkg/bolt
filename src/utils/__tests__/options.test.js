dec797bd5d61c9e3aba9b71d7555cc769abbd10e
Sendil Kumar <sendilkumarn@live.com>
2018-01-12 23:36:40 +0100

    Adding options test case

--- src/utils/__tests__/options.test.js
+++ src/utils/__tests__/options.test.js
@@ -1,7 +1,76 @@
 // @flow
 import * as options from '../options';
+import {
+  toDependency,
+  toYarnInit,
+  string,
+  boolean,
+  number,
+  toFilterOpts
+} from '../options';
 
 describe('options', () => {
-  test('string');
-  test('boolean');
+  test('string', () => {
+    expect(string(undefined, 'flag')).toBe();
+    expect(string('test', 'flag')).toBe('test');
+    expect(() => string(123, 'flag')).toThrow();
+  });
+
+  test('boolean', () => {
+    expect(boolean(true, 'flag')).toEqual(true);
+    expect(boolean(false, 'flag')).toEqual(false);
+    expect(() => boolean(123, 'flag')).toThrow();
+  });
+
+  test('number', () => {
+    expect(number(1, 'flag')).toEqual(1);
+    expect(number(2.2, 'flag')).toEqual(2.2);
+    expect(() => number('number', 'flag')).toThrow();
+  });
+
+  test('toFilerOptions', () => {
+    expect(toFilterOpts({ only: 'only' })).toEqual({ only: 'only' });
+    expect(toFilterOpts({ onlyFs: 'onlyFs' })).toEqual({ onlyFs: 'onlyFs' });
+    expect(toFilterOpts({ ignore: 'ignore' })).toEqual({ ignore: 'ignore' });
+    expect(toFilterOpts({ ignoreFs: 'ignoreFs' })).toEqual({
+      ignoreFs: 'ignoreFs'
+    });
+    expect(toFilterOpts({})).toEqual({});
+  });
+
+  test('toDependency', () => {
+    const name = 'packageName';
+    const version = '1.0.0';
+    expect(toDependency(name)).toEqual({ name });
+    let nameWithVersion = `packageName@${version}`;
+    expect(toDependency(nameWithVersion)).toEqual({ name, version });
+    nameWithVersion = `@packageName/${version}`;
+    expect(toDependency(nameWithVersion)).toEqual({ name: nameWithVersion });
+    nameWithVersion = `@packageName@${version}`;
+    expect(toDependency(nameWithVersion)).toEqual({ name, version });
+  });
+
+  test('toYarnInit', () => {
+    let flag = true;
+
+    expect(toYarnInit({})).toEqual({});
+
+    expect(toYarnInit({ y: flag })).toEqual({ yes: true });
+    expect(toYarnInit({ y: !flag })).toEqual({});
+    expect(toYarnInit({ yes: flag })).toEqual({ yes: true });
+    expect(toYarnInit({ yes: !flag })).toEqual({});
+    expect(toYarnInit({ private: flag })).toEqual({ private: true });
+    expect(toYarnInit({ private: !flag })).toEqual({});
+    expect(toYarnInit({ p: flag })).toEqual({ private: true });
+    expect(toYarnInit({ p: !flag })).toEqual({});
+
+    expect(toYarnInit({ y: flag, p: !flag })).toEqual({ yes: true });
+    expect(toYarnInit({ private: flag, y: !flag })).toEqual({ private: true });
+
+    expect(toYarnInit({ y: !flag, p: !flag })).toEqual({});
+    expect(toYarnInit({ y: flag, p: flag })).toEqual({
+      yes: true,
+      private: true
+    });
+  });
 });
