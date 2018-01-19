bd28ff343b107afd683db774e1d0b998b5c7320a
Sendil Kumar N <sendilkumarn@live.com>
2018-01-19 01:03:22 +0100

    Adding Project add <dependency> (#121)
    * adding add for project dependencies

--- src/commands/project/add.js
+++ src/commands/project/add.js
@@ -1,16 +1,47 @@
 // @flow
+import addDependenciesToPackage from '../../utils/addDependenciesToPackages';
+import Project from '../../Project';
 import * as options from '../../utils/options';
-import { BoltError } from '../../utils/errors';
+import * as logger from '../../utils/logger';
+import type { Dependency, configDependencyType } from '../../types';
+import { DEPENDENCY_TYPE_FLAGS_MAP } from '../../constants';
+import { add } from '../add';
 
-export type ProjectAddOptions = {};
+export type ProjectAddOptions = {
+  cwd?: string,
+  deps: Array<Dependency>,
+  type: configDependencyType
+};
 
 export function toProjectAddOptions(
   args: options.Args,
   flags: options.Flags
 ): ProjectAddOptions {
-  return {};
+  const depsArgs = [];
+  let type = 'dependencies';
+
+  // args is each of our dependencies we are adding which may have "@version" parts to them
+  args.forEach(dep => {
+    depsArgs.push(options.toDependency(dep));
+  });
+
+  Object.keys(DEPENDENCY_TYPE_FLAGS_MAP).forEach(depTypeFlag => {
+    if (flags[depTypeFlag]) {
+      type = DEPENDENCY_TYPE_FLAGS_MAP[depTypeFlag];
+      // check if value of dependency flag is a package name and then push to dependency arguments
+      if (typeof flags[depTypeFlag] === 'string') {
+        depsArgs.push(options.toDependency(flags[depTypeFlag]));
+      }
+    }
+  });
+
+  return {
+    cwd: options.string(flags.cwd, 'cwd'),
+    deps: depsArgs,
+    type
+  };
 }
 
 export async function projectAdd(opts: ProjectAddOptions) {
-  throw new BoltError('Unimplemented command "project add"');
+  await add(opts);
 }
