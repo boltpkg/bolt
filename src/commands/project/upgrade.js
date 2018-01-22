a0cc8db4076e018ab6b65eb62356b7e287035f0f
Sendil Kumar N <sendilkumarn@live.com>
2018-01-22 20:49:18 +0100

    replace pkg with project.pkg

--- src/commands/project/upgrade.js
+++ src/commands/project/upgrade.js
@@ -2,7 +2,6 @@
 import * as options from '../../utils/options';
 import { BoltError } from '../../utils/errors';
 import Project from '../../Project';
-import Package from '../../Package';
 import type { Dependency } from '../../types';
 import upgradeDependenciesInPackage from '../../utils/upgradeDependenciesInPackages';
 
@@ -48,10 +47,9 @@ export function toProjectUpgradeOptions(
 export async function projectUpgrade(opts: ProjectUpgradeOptions) {
   let cwd = opts.cwd || process.cwd();
   let project = await Project.init(cwd);
-  let pkg = await Package.closest(cwd);
 
   try {
-    await upgradeDependenciesInPackage(project, pkg, opts.deps, opts.flags);
+    await upgradeDependenciesInPackage(project, project.pkg, opts.deps, opts.flags);
   } catch (err) {
     throw new BoltError(`upgrading dependencies failed due to ${err}`);
   }
