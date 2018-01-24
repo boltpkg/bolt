ac792f1ae5c61badf917ac47dba820968ecba398
Raja Sekar <rajasekar89be@gmail.com>
2018-01-24 14:18:04 +0530

    test case for bolt info

--- src/commands/__tests__/info.test.js
+++ src/commands/__tests__/info.test.js
@@ -5,33 +5,28 @@ import * as yarn from '../../utils/yarn';
 
 jest.mock('../../utils/yarn');
 
+const cwd = 'dummy/path/to/dir';
+const args = ['react'];
+const flags = { cwd, json: true };
+const opt = { flags, args };
+
 describe('bolt info', () => {
   test('should return the expected cwd', () => {
-    const args = [];
-    const cwd = 'dummy/path/to/dir';
-    const flags = { cwd };
     const result = toInfoOptions(args, flags);
     expect(result.cwd).toBe(cwd);
   });
 
   test('should return the expected argument', () => {
-    const args = ['react'];
-    const flags = {};
     const result = toInfoOptions(args, flags);
     expect(result.args).toBe(args);
   });
 
   test('should return the expected flags', () => {
-    const commandArgs = [];
-    const flags = { json: true };
-    const result = toInfoOptions(commandArgs, flags);
+    const result = toInfoOptions(args, flags);
     expect(result.flags).toBe(flags);
   });
 
   test('should call npm login once', async () => {
-    const cwd = 'dummy/path/to/dir';
-    const args = ['react'];
-    const opt = { cwd, args };
     await info(opt);
     expect(yarn.info).toHaveBeenCalledTimes(1);
   });
