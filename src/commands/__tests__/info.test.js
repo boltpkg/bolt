81186e1a0a7678056a72817952d332b6b4bd5445
Raja Sekar <rajasekar89be@gmail.com>
2018-01-24 18:29:26 +0530

    Test case for bolt info (#133)
    * test case for bolt info

--- src/commands/__tests__/info.test.js
+++ src/commands/__tests__/info.test.js
@@ -1,4 +1,33 @@
 // @flow
 import { info, toInfoOptions } from '../info';
 
-test('bolt info');
+import * as yarn from '../../utils/yarn';
+
+jest.mock('../../utils/yarn');
+
+const cwd = 'dummy/path/to/dir';
+const args = ['react'];
+const flags = { cwd, json: true };
+const opt = { flags, args };
+
+describe('bolt info', () => {
+  test('should return the expected cwd', () => {
+    const result = toInfoOptions(args, flags);
+    expect(result.cwd).toBe(cwd);
+  });
+
+  test('should return the expected argument', () => {
+    const result = toInfoOptions(args, flags);
+    expect(result.args).toBe(args);
+  });
+
+  test('should return the expected flags', () => {
+    const result = toInfoOptions(args, flags);
+    expect(result.flags).toBe(flags);
+  });
+
+  test('should call npm login once', async () => {
+    await info(opt);
+    expect(yarn.info).toHaveBeenCalledTimes(1);
+  });
+});
