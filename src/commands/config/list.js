6c9b8874da3baa57fa441b937212478ea0d68b38
Sendil Kumar <sendilkumarn@live.com>
2018-01-25 18:11:03 +0100

    Adding bolt config list command

--- src/commands/config/list.js
+++ src/commands/config/list.js
@@ -1,6 +1,7 @@
 // @flow
 import * as options from '../../utils/options';
 import { BoltError } from '../../utils/errors';
+import * as yarn from '../../utils/yarn';
 
 export type ConfigListOptions = {};
 
@@ -8,9 +9,17 @@ export function toConfigListOptions(
   args: options.Args,
   flags: options.Flags
 ): ConfigListOptions {
+  if (args.length !== 0 || flags['--'].length !== 0) {
+    throw new BoltError('Invalid subcommand, try bolt config list');
+  }
   return {};
 }
 
 export async function configList(opts: ConfigListOptions) {
-  throw new BoltError('Unimplemented command "config list"');
+  let cwd = opts.cwd || process.cwd();
+  try {
+    await yarn.cliCommand(cwd, 'config list');
+  } catch (err) {
+    throw new BoltError(err);
+  }
 }
