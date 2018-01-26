5b77b3c4f2cfbadbb6d7186b6fef1d2895c25a6f
Sendil Kumar N <sendilkumarn@live.com>
2018-01-26 08:01:03 +0100

    Adding bolt config list command (#134)
    * Adding bolt config list command
    
    * fixing flow types  :(
    
    * fixing the flags and cli command call
    
    * removes unused dependencies

--- src/commands/config/__tests__/list.test.js
+++ src/commands/config/__tests__/list.test.js
@@ -1,4 +1,12 @@
 // @flow
 import { configList, toConfigListOptions } from '../list';
+import * as yarn from '../../../utils/yarn';
+import { BoltError } from '../../../utils/errors';
 
-test('bolt config list');
+jest.mock('../../../utils/yarn');
+
+const dummyPath = '/dummyPattern/dummyPath';
+test('bolt config list', async () => {
+  let config = await configList({ cwd: dummyPath });
+  expect(yarn.cliCommand).toHaveBeenCalledWith(dummyPath, 'config', ['list']);
+});
--- src/commands/config/list.js
+++ src/commands/config/list.js
@@ -1,16 +1,27 @@
 // @flow
 import * as options from '../../utils/options';
 import { BoltError } from '../../utils/errors';
+import * as yarn from '../../utils/yarn';
 
-export type ConfigListOptions = {};
+export type ConfigListOptions = {
+  cwd?: string
+};
 
 export function toConfigListOptions(
   args: options.Args,
   flags: options.Flags
 ): ConfigListOptions {
-  return {};
+  if (args.length !== 0) {
+    throw new BoltError('Invalid subcommand, try bolt config list');
+  }
+  return { cwd: options.string(flags.cwd, 'cwd') };
 }
 
 export async function configList(opts: ConfigListOptions) {
-  throw new BoltError('Unimplemented command "config list"');
+  let cwd = opts.cwd || process.cwd();
+  try {
+    await yarn.cliCommand(cwd, 'config', ['list']);
+  } catch (err) {
+    throw new BoltError(err);
+  }
 }
