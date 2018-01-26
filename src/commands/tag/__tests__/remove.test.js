07cb5a7742ee72d2afa8b6575d79c88fa78be825
Sendil Kumar <sendilkumarn@live.com>
2018-01-26 23:08:07 +0100

    Adds tag remove

--- src/commands/tag/__tests__/remove.test.js
+++ src/commands/tag/__tests__/remove.test.js
@@ -1,4 +1,18 @@
 // @flow
 import { tagRemove, toTagRemoveOptions } from '../remove';
+import * as yarn from '../../../utils/yarn';
+import { BoltError } from '../../../utils/errors';
 
-test('bolt tag remove');
+jest.mock('../../../utils/yarn');
+
+const dummyPath = '/dummyPattern/dummyPath';
+test('bolt tag remove', async () => {
+  const tag = await tagRemove(
+    toTagRemoveOptions(['test-tag', 'stable'], { cwd: dummyPath })
+  );
+  expect(yarn.cliCommand).toHaveBeenCalledWith(dummyPath, 'tag', [
+    'remove',
+    'test-tag',
+    'stable'
+  ]);
+});
