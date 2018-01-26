affa49daae4cd74de7c6e510667c74c35e49cd2c
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2018-01-26 18:19:48 +1100

    implenent bolt import

--- src/commands/__tests__/import.test.js
+++ src/commands/__tests__/import.test.js
@@ -1,4 +1,14 @@
 // @flow
 import { import_, toImportOptions } from '../import';
+import * as yarn from '../../utils/yarn';
 
-test('bolt import');
+jest.mock('../../utils/yarn');
+
+const projectDirMock = 'dummyPattern/dummyPath';
+
+describe('bolt import', () => {
+  it('should call yarn cliCommand with autoClean and path to project', async () => {
+    await import_(toImportOptions([], { cwd: projectDirMock }));
+    expect(yarn.cliCommand).toHaveBeenCalledWith(projectDirMock, 'import');
+  });
+});
--- src/commands/import.js
+++ src/commands/import.js
@@ -1,16 +1,27 @@
 // @flow
 import * as options from '../utils/options';
 import { BoltError } from '../utils/errors';
+import * as yarn from '../utils/yarn';
 
-export type ImportOptions = {};
+export type ImportOptions = {
+  cwd?: string
+};
 
 export function toImportOptions(
   args: options.Args,
   flags: options.Flags
 ): ImportOptions {
-  return {};
+  return {
+    cwd: options.string(flags.cwd, 'cwd')
+  };
 }
 
 export async function import_(opts: ImportOptions) {
-  throw new BoltError('Unimplemented command "import"');
+  let cwd = opts.cwd || process.cwd();
+
+  try {
+    await yarn.cliCommand(cwd, 'import');
+  } catch (err) {
+    throw new BoltError(err);
+  }
 }
