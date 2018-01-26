01f65e2b59014fd42d0da1b945aa19ce8623a956
Sendil Kumar <sendilkumarn@live.com>
2018-01-26 23:07:58 +0100

    Adds tag list

--- src/commands/tag/__tests__/list.test.js
+++ src/commands/tag/__tests__/list.test.js
@@ -1,4 +1,15 @@
 // @flow
 import { tagList, toTagListOptions } from '../list';
+import * as yarn from '../../../utils/yarn';
+import { BoltError } from '../../../utils/errors';
 
-test('bolt tag list');
+jest.mock('../../../utils/yarn');
+
+const dummyPath = '/dummyPattern/dummyPath';
+test('bolt tag list', async () => {
+  const tag = await tagList(toTagListOptions(['react'], { cwd: dummyPath }));
+  expect(yarn.cliCommand).toHaveBeenCalledWith(dummyPath, 'tag', [
+    'list',
+    'react'
+  ]);
+});
--- src/commands/tag/list.js
+++ src/commands/tag/list.js
@@ -1,16 +1,27 @@
 // @flow
 import * as options from '../../utils/options';
 import { BoltError } from '../../utils/errors';
+import * as yarn from '../../utils/yarn';
 
-export type TagListOptions = {};
+export type TagListOptions = {
+  cwd?: string,
+  args: Array<string>
+};
 
 export function toTagListOptions(
   args: options.Args,
   flags: options.Flags
 ): TagListOptions {
-  return {};
+  return { cwd: options.string(flags.cwd, 'cwd'), args };
 }
 
 export async function tagList(opts: TagListOptions) {
-  throw new BoltError('Unimplemented command "tag list"');
+  let cwd = opts.cwd || process.cwd();
+  let args = opts.args || [];
+
+  try {
+    await yarn.cliCommand(cwd, 'tag', ['list', ...args]);
+  } catch (err) {
+    throw new BoltError(err);
+  }
 }
