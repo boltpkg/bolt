b73246cbc408b851b9fbca2ea6bbd7d7ea43122a
Sendil Kumar <sendilkumarn@live.com>
2018-01-26 13:35:40 +0100

    Adding config delete option

--- src/commands/config/__tests__/delete.test.js
+++ src/commands/config/__tests__/delete.test.js
@@ -1,4 +1,17 @@
 // @flow
 import { configDelete, toConfigDeleteOptions } from '../delete';
+import * as yarn from '../../../utils/yarn';
+import { BoltError } from '../../../utils/errors';
 
-test('bolt config delete');
+jest.mock('../../../utils/yarn');
+
+const dummyPath = '/dummyPattern/dummyPath';
+test('bolt config get', async () => {
+  const config = await configDelete(
+    toConfigDeleteOptions(['user-agent'], { cwd: dummyPath })
+  );
+  expect(yarn.cliCommand).toHaveBeenCalledWith(dummyPath, 'config', [
+    'delete',
+    'user-agent'
+  ]);
+});
