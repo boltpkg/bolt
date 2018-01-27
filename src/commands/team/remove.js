3b715e4090977cdfc99b58774f8138eadae526aa
Sendil Kumar <sendilkumarn@live.com>
2018-01-28 00:05:27 +0100

    Adds team remove

--- src/commands/team/remove.js
+++ src/commands/team/remove.js
@@ -1,16 +1,25 @@
 // @flow
 import * as options from '../../utils/options';
+import * as npm from '../../utils/npm';
 import { BoltError } from '../../utils/errors';
 
-export type TeamRemoveOptions = {};
+export type TeamRemoveOptions = {
+  cwd?: string,
+  args: Array<string>
+};
 
 export function toTeamRemoveOptions(
   args: options.Args,
   flags: options.Flags
 ): TeamRemoveOptions {
-  return {};
+  return { cwd: options.string(flags.cwd, 'cwd'), args };
 }
 
 export async function teamRemove(opts: TeamRemoveOptions) {
-  throw new BoltError('Unimplemented command "team remove"');
+  let cwd = opts.cwd || process.cwd();
+  try {
+    await npm.cliCommand(cwd, 'team', ['rm', ...opts.args]);
+  } catch (err) {
+    throw new BoltError(err);
+  }
 }
