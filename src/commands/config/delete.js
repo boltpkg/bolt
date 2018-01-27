ec003bec23ecbb8b10804c5c5f24828977c55da1
Sendil Kumar N <sendilkumarn@live.com>
2018-01-27 02:50:03 +0100

    Adds Config delete (#138)
    * Adding config delete option
    
    * fixes word

--- src/commands/config/delete.js
+++ src/commands/config/delete.js
@@ -1,16 +1,25 @@
 // @flow
 import * as options from '../../utils/options';
 import { BoltError } from '../../utils/errors';
+import * as yarn from '../../utils/yarn';
 
-export type ConfigDeleteOptions = {};
+export type ConfigDeleteOptions = {
+  cwd?: string,
+  args: Array<string>
+};
 
 export function toConfigDeleteOptions(
   args: options.Args,
   flags: options.Flags
 ): ConfigDeleteOptions {
-  return {};
+  return { cwd: options.string(flags.cwd, 'cwd'), args };
 }
 
 export async function configDelete(opts: ConfigDeleteOptions) {
-  throw new BoltError('Unimplemented command "config delete"');
+  let cwd = opts.cwd || process.cwd();
+  try {
+    await yarn.cliCommand(cwd, 'config', ['delete', ...opts.args]);
+  } catch (err) {
+    throw new BoltError(err);
+  }
 }
