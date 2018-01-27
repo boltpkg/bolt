5becbcb8b772f36b002216b9c5fb90cc1bdf1117
Sendil Kumar <sendilkumarn@live.com>
2018-01-28 00:05:19 +0100

    Adds team list

--- src/commands/team/__tests__/list.test.js
+++ src/commands/team/__tests__/list.test.js
@@ -1,4 +1,17 @@
 // @flow
 import { teamList, toTeamListOptions } from '../list';
+import * as npm from '../../../utils/npm';
+import { BoltError } from '../../../utils/errors';
 
-test('bolt team list');
+jest.mock('../../../utils/npm');
+
+const dummyPath = '/dummyPattern/dummyPath';
+test('bolt team list', async () => {
+  const team = await teamList(
+    toTeamListOptions(['test-tag'], { cwd: dummyPath })
+  );
+  expect(npm.cliCommand).toHaveBeenCalledWith(dummyPath, 'team', [
+    'ls',
+    'test-tag'
+  ]);
+});
