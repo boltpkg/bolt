8315dc8f09abccfa6f6d53572c22dcf36d82f1af
Sendil Kumar <sendilkumarn@live.com>
2018-01-28 00:05:04 +0100

    Adds team create

--- src/commands/team/create.js
+++ src/commands/team/create.js
@@ -1,16 +1,25 @@
 // @flow
 import * as options from '../../utils/options';
+import * as npm from '../../utils/npm';
 import { BoltError } from '../../utils/errors';
 
-export type TeamCreateOptions = {};
+export type TeamCreateOptions = {
+  cwd?: string,
+  args: Array<string>
+};
 
 export function toTeamCreateOptions(
   args: options.Args,
   flags: options.Flags
 ): TeamCreateOptions {
-  return {};
+  return { cwd: options.string(flags.cwd, 'cwd'), args };
 }
 
 export async function teamCreate(opts: TeamCreateOptions) {
-  throw new BoltError('Unimplemented command "team create"');
+  let cwd = opts.cwd || process.cwd();
+  try {
+    await npm.cliCommand(cwd, 'team', ['create', ...opts.args]);
+  } catch (err) {
+    throw new BoltError(err);
+  }
 }
