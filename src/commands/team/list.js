da071f078f3158c32f58fbcc511bd4a671084edc
Sendil Kumar <sendilkumarn@live.com>
2018-01-28 00:05:19 +0100

    Adds team list

--- src/commands/team/list.js
+++ src/commands/team/list.js
@@ -1,16 +1,25 @@
 // @flow
 import * as options from '../../utils/options';
+import * as npm from '../../utils/npm';
 import { BoltError } from '../../utils/errors';
 
-export type TeamListOptions = {};
+export type TeamListOptions = {
+  cwd?: string,
+  args: Array<string>
+};
 
 export function toTeamListOptions(
   args: options.Args,
   flags: options.Flags
 ): TeamListOptions {
-  return {};
+  return { cwd: options.string(flags.cwd, 'cwd'), args };
 }
 
 export async function teamList(opts: TeamListOptions) {
-  throw new BoltError('Unimplemented command "team list"');
+  let cwd = opts.cwd || process.cwd();
+  try {
+    await npm.cliCommand(cwd, 'team', ['ls', ...opts.args]);
+  } catch (err) {
+    throw new BoltError(err);
+  }
 }
