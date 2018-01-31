392f71114acad7557730b098aa3acd4bbdcf143b
Raja Bellebon <rbellebon@gmail.com>
2018-02-01 09:29:35 +1100

    Revert "adding the add command + tests"
    This reverts commit 1044eb0c237a402d0b33e72c5836dbe8d81d2953.

--- src/commands/owner/__tests__/add.test.js
+++ src/commands/owner/__tests__/add.test.js
@@ -1,18 +1,4 @@
 // @flow
 import { ownerAdd, toOwnerAddOptions } from '../add';
-import * as yarn from '../../../utils/yarn';
-import { BoltError } from '../../../utils/errors';
 
-jest.mock('../../../utils/yarn');
-
-const dummyPath = '/dummyPattern/dummyPath';
-test('bolt owner add', async () => {
-  const tag = await ownerAdd(
-    toOwnerAddOptions(['test-owner@1.0.0', 'stable'], { cwd: dummyPath })
-  );
-  expect(yarn.cliCommand).toHaveBeenCalledWith(dummyPath, 'owner', [
-    'add',
-    'test-owner@1.0.0',
-    'stable'
-  ]);
-});
+test('bolt owner add');
--- src/commands/owner/add.js
+++ src/commands/owner/add.js
@@ -1,25 +1,16 @@
 // @flow
 import * as options from '../../utils/options';
-import * as yarn from '../../utils/yarn';
 import { BoltError } from '../../utils/errors';
 
-export type OwnerAddOptions = {
-  cwd?: string,
-  args: Array<string>
-};
+export type OwnerAddOptions = {};
 
 export function toOwnerAddOptions(
   args: options.Args,
   flags: options.Flags
 ): OwnerAddOptions {
-  return { cwd: options.string(flags.cwd, 'cwd'), args };
+  return {};
 }
 
 export async function ownerAdd(opts: OwnerAddOptions) {
-  let cwd = opts.cwd || process.cwd();
-  try {
-    await yarn.cliCommand(cwd, 'owner', ['add', ...opts.args]);
-  } catch (err) {
-    throw new BoltError(err);
-  }
+  throw new BoltError('Unimplemented command "owner add"');
 }
