30ee4cab5f28f374e6e373ff3c76aab1b202ff34
Sendil Kumar <sendilkumarn@live.com>
2018-02-03 20:13:40 +0100

    Adding global upgrade

--- src/commands/global/upgrade.js
+++ src/commands/global/upgrade.js
@@ -1,16 +1,33 @@
 // @flow
 import * as options from '../../utils/options';
+import * as yarn from '../../utils/yarn';
+import type { Dependency } from '../../types';
 import { BoltError } from '../../utils/errors';
 
-export type GlobalUpgradeOptions = {};
+export type GlobalUpgradeOptions = {
+  deps: Array<Dependency>
+};
 
 export function toGlobalUpgradeOptions(
   args: options.Args,
   flags: options.Flags
 ): GlobalUpgradeOptions {
-  return {};
+  const depsArgs = [];
+
+  // args is each of our dependencies we are adding which may have "@version" parts to them
+  args.forEach(dep => {
+    depsArgs.push(options.toDependency(dep));
+  });
+
+  return {
+    deps: depsArgs
+  };
 }
 
 export async function globalUpgrade(opts: GlobalUpgradeOptions) {
-  throw new BoltError('Unimplemented command "global upgrade"');
+  try {
+    await yarn.globalCli('upgrade', opts.deps);
+  } catch (err) {
+    throw new BoltError(err);
+  }
 }
