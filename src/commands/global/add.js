7a5f2d7fb9e66b4dd2f39260cf3655827d7750d5
Sendil Kumar <sendilkumarn@live.com>
2018-02-03 01:12:03 +0100

    adding global add

--- src/commands/global/add.js
+++ src/commands/global/add.js
@@ -1,16 +1,33 @@
 // @flow
 import * as options from '../../utils/options';
+import * as yarn from '../../utils/yarn';
+import type { Dependency } from '../../types';
 import { BoltError } from '../../utils/errors';
 
-export type GlobalAddOptions = {};
+export type GlobalAddOptions = {
+  deps: Array<Dependency>
+};
 
 export function toGlobalAddOptions(
   args: options.Args,
   flags: options.Flags
 ): GlobalAddOptions {
-  return {};
+  const depsArgs = [];
+
+  // args is each of our dependencies we are adding which may have "@version" parts to them
+  args.forEach(dep => {
+    depsArgs.push(options.toDependency(dep));
+  });
+
+  return {
+    deps: depsArgs
+  };
 }
 
 export async function globalAdd(opts: GlobalAddOptions) {
-  throw new BoltError('Unimplemented command "global add"');
+  try {
+    await yarn.globalCli('add', opts.deps);
+  } catch (err) {
+    throw new BoltError(err);
+  }
 }
