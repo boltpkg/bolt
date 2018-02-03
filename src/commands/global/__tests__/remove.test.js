b06e5a3b2f61d02552793526f456e1fbd53bed2a
Sendil Kumar <sendilkumarn@live.com>
2018-02-03 20:13:31 +0100

    Adding global remove

--- src/commands/global/__tests__/remove.test.js
+++ src/commands/global/__tests__/remove.test.js
@@ -1,4 +1,14 @@
 // @flow
 import { globalRemove, toGlobalRemoveOptions } from '../remove';
+import * as yarn from '../../../utils/yarn';
+import { BoltError } from '../../../utils/errors';
 
-test('bolt global remove');
+jest.mock('../../../utils/yarn');
+
+const dummyPath = '/dummyPattern/dummyPath';
+test('bolt global remove', async () => {
+  const tag = await globalRemove(
+    toGlobalRemoveOptions(['test-tag'], { cwd: dummyPath })
+  );
+  expect(yarn.globalCli).toHaveBeenCalledWith('remove', [{ name: 'test-tag' }]);
+});
