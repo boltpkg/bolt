69c1c55ba5aa007906e28156471e0f1988a00c57
Luke Batchelor <lbatchelor@atlassian.com>
2018-09-14 15:39:41 +1000

    Overrides the npm_config_registry var when doing info calls

--- src/utils/npm.js
+++ src/utils/npm.js
@@ -11,8 +11,19 @@ export function info(pkgName: string) {
   return npmRequestLimit(async () => {
     logger.info(messages.npmInfo(pkgName));
 
+    // Due to a couple of issues with yarnpkg, we also want to override the npm registry when doing
+    // npm info.
+    // Issues: We sometimes get back cached responses, i.e old data about packages which causes
+    // `publish` to behave incorrectly. It can also cause issues when publishing private packages
+    // as they will always give a 404, which will tell `publish` to always try to publish.
+    // See: https://github.com/yarnpkg/yarn/issues/2935#issuecomment-355292633
+    const envOverride = {
+      npm_config_registry: 'https://registry.npmjs.org/'
+    };
+
     let result = await processes.spawn('npm', ['info', pkgName, '--json'], {
-      silent: true
+      silent: true,
+      env: Object.assign({}, process.env, envOverride)
     });
 
     return JSON.parse(result.stdout);
