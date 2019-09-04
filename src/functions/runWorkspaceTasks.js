ee42b0003a525982f1d366c56a2f36b0ca95b9f3
Michael Blaszczyk <mblaszczyk@atlassian.com>
2019-09-04 11:58:00 +1000

    Add filterOpts option to runWorkspaceTasks function

--- src/functions/runWorkspaceTasks.js
+++ src/functions/runWorkspaceTasks.js
@@ -1,6 +1,6 @@
 // @flow
 import Project from '../Project';
-import type { JSONValue, SpawnOpts } from '../types';
+import type { FilterOpts, JSONValue, SpawnOpts } from '../types';
 
 type WorkspaceInfo = {
   dir: string,
@@ -11,7 +11,8 @@ type Task = (workspace: WorkspaceInfo) => Promise<mixed>;
 
 type Options = {
   cwd?: string,
-  spawnOpts?: SpawnOpts
+  spawnOpts?: SpawnOpts,
+  filterOpts?: FilterOpts
 };
 
 export default async function runWorkspaceTasks(
@@ -20,10 +21,12 @@ export default async function runWorkspaceTasks(
 ): Promise<void> {
   let cwd = opts.cwd || process.cwd();
   let spawnOpts = opts.spawnOpts || {};
+  let filterOpts = opts.filterOpts || {};
   let project = await Project.init(cwd);
   let packages = await project.getPackages();
+  let filteredPackages = project.filterPackages(packages, filterOpts);
 
-  await project.runPackageTasks(packages, spawnOpts, pkg => {
+  await project.runPackageTasks(filteredPackages, spawnOpts, pkg => {
     return task({
       dir: pkg.dir,
       config: pkg.config.json
